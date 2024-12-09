b321caf01d2a2caaa409412e658a481a967310c2
THEOPHILE PICOLET <theophilepicolet@hotmail.fr>
2024-12-09 10:51:04 +0100

    wilders

---
+++ index.mjs
@@ -0,0 +1,13 @@
+import chalk from "chalk";
+
+const lisa = "Lisa";
+const Julien = "Julien";
+const Julian = "Julian";
+const Brayan= "Brayan";
+
+console.log(
+  chalk.red(lisa),
+  chalk.cyan(Julien),
+  chalk.white(Julian),
+  chalk.yellow(Brayan)
+);
